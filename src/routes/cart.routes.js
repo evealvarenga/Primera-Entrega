f64a3383a8a0bc1d4e68d19bd21e2b03edb379fb
evealvarenga <ealvarenga095@gmail.com>
2023-09-27 10:20:41 -0300

    Se agrega lógica en los endpoints del cart

--- src/routes/cart.routes.js
+++ src/routes/cart.routes.js
@@ -7,13 +7,23 @@ const router = Router();
 
 //Endpoint para crear un nuevo carrito
 router.post('/', async (req, res) => {
-    
+    try {
+        const cart = await manager.addCart();
+        return res.status(200).json({message: "Carrito creado.", cart});
+    } catch (error) {
+        res.status(500).json({ error: 'Hubo un error al obtener los productos del carrito.' });
+    }
 });
 
 //Endpoint para obtener productos del carrito
 router.get('/:cid', async (req, res) => {
+    const { cid } = req.params;
     try {
-        //AGREGAR PARTE LÓGICA
+        const cart = await manager.getCartByID(+cid);
+        if (!cart) {
+            return res.status(404).json({message: "Carrito no encontrado."});
+        } 
+        return res.status(200).json({message: "Carrito encontrado.", cart});
     } catch (error) {
         res.status(500).json({ error: 'Hubo un error al obtener los productos del carrito.' });
     }
@@ -21,9 +31,11 @@ router.get('/:cid', async (req, res) => {
 
 //Endpoint para agregar un producto al carrito
 router.post('/:cid/product/:pid', async (req, res) => {
+    const { cid, pid } = req.params;
+    const { quantity } = req.body;
     try {
-        const { cid, pid } = req.params;
-        const { quantity } = req.body;
+        const response = await manager.addProductToCart(req.params, req.body);
+        res.status(200).json({message:"Productos agregados al carrito", Carrito: response})
     } catch (error) {
         res.status(500).json({ error: 'Hubo un error al agregar el producto al carrito.' });
     }
